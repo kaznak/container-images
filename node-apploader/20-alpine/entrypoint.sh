4e2dcd090fa762ee1d64087df475c1456d315b27
kaznak <kaznak.at.work@ipebble.org>
2024-06-20 22:30:46 +0900

    update

--- node-apploader/20-alpine/entrypoint.sh
+++ node-apploader/20-alpine/entrypoint.sh
@@ -1,33 +1,64 @@
 #!/bin/sh
 
+set -Cu
 set -e
-set -u
 
 # setup ssh
-mkdir ~/.ssh
-echo "${GIT_SSH_KEY}"    > ~/.ssh/id
-chmod -R go-rwx ~/.ssh
+mkdir -p ~/.ssh
+if [ "dummy" != "${GIT_SSH_KEY:-"dummy"}" ] ; then
+    echo "${GIT_SSH_KEY}"    > ~/.ssh/id_rsa
+    chmod -R go-rwx ~/.ssh
+fi
+
+set -vx
 
 # clone app
 git clone "${GIT_REPOSITORY}" app
 cd app
 
-# build and install
-npm install --verbose
-npm run build
-npm install --omit dev
-
-# run
-if [ "$@" ] ; then
-    exec npm run "$@"
+# check package manager
+if [ -s "./package-lock.json" ] ; then
+    NPM=${NPM:-"npm"}
+elif [ -s "./yarn.lock" ] ; then
+    NPM=${NPM:-"yarn"}
+elif [ -s "./pnpm-lock.yaml" ] ; then
+    NPM=${NPM:-"pnpm"}
 else
-    case "$NODE_ENV" in
-        production)
-            exec npm run start
-            ;;
-        # development)
-        *)
-            exec npm run dev
-            ;;
-    esac
+    echo "not supported package manager" >&2
+    exit 1
 fi
+
+case "$NPM" in
+    yarn)
+        corepack enable yarn
+        echo "yarn does not supported" >&2
+        exit 1
+        ;;
+    pnpm)
+        corepack enable pnpm
+        echo "pnpm does not supported" >&2
+        exit 1
+        ;;
+    # npm)
+    *)
+        # build and install
+        npm install --verbose --include dev
+        npm run build
+
+        # run
+        if [ "$@" ] ; then
+            exec npm run "$@"
+        else
+            case "$NODE_ENV" in
+                production)
+                    npm install --omit dev
+                    exec npm run start
+                    ;;
+                # development)
+                *)
+                    exec npm run dev
+                    ;;
+            esac
+        fi
+        ;;
+esac
