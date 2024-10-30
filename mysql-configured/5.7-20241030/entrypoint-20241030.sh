3fd3fde70f456aaaacd64a3926d1d8532a8a98aa
kaznak <kaznak.at.work@ipebble.org>
2024-10-30 13:44:55 +0900

    fix

--- mysql-configured/5.7-20241030/entrypoint-20241030.sh
+++ mysql-configured/5.7-20241030/entrypoint-20241030.sh
@@ -4,4 +4,4 @@ set -eo pipefail
 shopt -s nullglob
 
 source /entrypoint.sh
-_main --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci --transaction-isolation=READ-COMMITTED --enforce-gtid-consistency=ON "$@"
+_main --character-set-server=utf8mb4 --collation-server=utf8mb4_general_ci --transaction-isolation=READ-COMMITTED --enforce-gtid-consistency=ON "$@"
